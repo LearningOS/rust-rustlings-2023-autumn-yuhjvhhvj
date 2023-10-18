e19ac80889699d97ec9c0bb01036bffa95032fd9
yuhjvhhvj <96415253+yuhjvhhvj@users.noreply.github.com>
2023-10-18 11:31:06 +0000

    update

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -3,13 +3,14 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    call_me();
+    let a:u32=10;
+    call_me(a);
 }
 
-fn call_me(num: u32) {
+fn call_me(num: u32)->() {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
