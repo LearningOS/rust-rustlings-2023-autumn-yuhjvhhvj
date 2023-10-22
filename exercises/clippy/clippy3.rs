ac4360c7cf6a65f30b39ab0082a187ea7bb70c8d
yuhjvhhvj <96415253+yuhjvhhvj@users.noreply.github.com>
2023-10-22 11:41:26 +0000

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -6,12 +6,12 @@
 
 
 
-#[allow(unused_variables, unused_assignments, clippy::panicking_unwrap ,clippy::unnecessary_literal_unwrap)]
+#[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
 
     if my_option.is_none() {
-        my_option.unwrap();
+        //my_option.unwrap();
     }
 
     let my_arr = &[
@@ -26,9 +26,7 @@ fn main() {
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
-    //value_a = value_b;
-    //value_b = value_a;
+
     std::mem::swap(&mut value_a,&mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
